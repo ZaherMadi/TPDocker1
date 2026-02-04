9e64601ba1f99c6438ddc03cda3dc242988a187e
Zaher Madi <zahermadi@yahoo.fr>
2026-02-04 10:28:49 +0100

    Refactor workflows to use lowercase repository names for GHCR compatibility and add Terraform configuration for AWS resources

---
+++ main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = "eu-west-3" # Paris
+}
+
+# 1. AMI Ubuntu 24.04
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+# 2. Creation de la cle SSH
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "registry-key-terraform"
+  public_key = tls_private_key.pk.public_key_openssh
+}
+
+# Sauvegarde de la cle privee locale pour Ansible
+resource "local_file" "ssh_key" {
+  filename        = "${path.module}/registry-key-terraform.pem"
+  content         = tls_private_key.pk.private_key_pem
+  file_permission = "0400"
+}
+
+# 3. Security group pour SSH / UI / Registry
+resource "aws_security_group" "registry_sg" {
+  name        = "registry-sg-simple"
+  description = "Allow SSH, HTTP (UI), Registry (5000)"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Registry UI"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Registry Docker API"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 4. Instance EC2
+resource "aws_instance" "registry_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.generated_key.key_name
+
+  vpc_security_group_ids = [aws_security_group.registry_sg.id]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Terraform-Registry-Server"
+  }
+}
+
+# 5. Output (Pour recuperer l'IP facilement)
+output "instance_ip" {
+  value = aws_instance.registry_server.public_ip
+}
